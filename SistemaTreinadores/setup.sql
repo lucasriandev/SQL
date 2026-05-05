3ebbbca1f714e0b80855ba1b25a8a6bd0bb9db3a
Lucas <lucasrian.dev@gmail.com>
2026-05-05 13:39:42 -0300

    tabela criada

---
+++ SistemaTreinadores/setup.sql
@@ -0,0 +1,19 @@
+    CREATE TABLE treinadores (
+        id SERIAL PRIMARY KEY,
+        nome_treinador VARCHAR(20)
+    );
+
+    CREATE TABLE pokemons(
+        id SERIAL PRIMARY KEY,
+        nomePokemon VARCHAR(50),
+        tipo VARCHAR(20),
+        treinador_id INT,
+        CONSTRAINT tt_treinador FOREIGN KEY (treinador_id) REFERENCES treinadores(id)
+    );
+
+    INSERT INTO treinadores(nome_treinador)
+    VALUES ('ASH')
+
+    INSERT INTO pokemons(nomePokemon, tipo, treinador_id)
+    VALUES ('Pikachu', 'Eletrico', 1)
+
