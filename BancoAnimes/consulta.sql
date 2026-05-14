949c7ff8018ccb30a99b351b3d4283899029e592
Lucas <lucasrian.dev@gmail.com>
2026-05-14 11:31:34 -0300

    treinando filtros

--- BancoAnimes/consulta.sql
+++ BancoAnimes/consulta.sql
@@ -1,6 +1,17 @@
-SELECT a.nome_anime, COUNT(p.id) AS total_personagens
-FROM animes a
-JOIN personagem p 
-ON a.id = p.anime_id
-GROUP BY a.nome_anime
-HAVING COUNT(p.id) > 2;
+SELECT N.nome_anime, COUNT(j.id) AS total
+FROM animes N
+JOIN personagem j ON N.id = j.anime_id
+WHERE j.personagemAnime != 'SASUKE'
+GROUP BY N.nome_anime
+HAVING COUNT(j.id) > 1;
+
+
+/*FROM / JOIN: Onde estão os dados?
+
+WHERE: Quem eu ignoro agora? (Linhas)
+
+GROUP BY: Como eu agrupo o que sobrou?
+
+HAVING: Quais grupos eu ignoro? (Totais)
+
+SELECT: O que eu mostro na tela?*/
